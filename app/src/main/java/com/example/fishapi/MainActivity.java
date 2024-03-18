529dfec830a70f843da6f459b5751acce94f0180
Nagy Robert <robi20010713@gmail.com>
2024-03-18 14:18:47 +0100

    Fixes v1
    touched:
    - home fragment created
    - login fragment created
    - signup fragment created
    - moved the sigup's xml to the actual signup fragment
    - main toolbar and navigation created
    
    bugs:
    - when entering landscape mode within the signup fragment, the application crashes

--- app/src/main/java/com/example/fishapi/MainActivity.java
+++ app/src/main/java/com/example/fishapi/MainActivity.java
@@ -1,24 +1,70 @@
 package com.example.fishapi;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
+    public NavController navController() {
+        return navController;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_home){
+            setTitle("Home");
+            navController.navigate(R.id.homeFragment);
+        } else if (id == R.id.action_login) {
+            setTitle("Sign In");
+            navController.navigate(R.id.loginFragment);
+        } else if (id == R.id.action_signup) {
+            setTitle("Sign Up");
+            navController.navigate(R.id.signupFragment);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT)
+                .show();
     }
 }
