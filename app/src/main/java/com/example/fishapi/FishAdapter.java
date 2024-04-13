d2ce43dfa5a605454098f56c711b0d3103762574
Kalmár Patrik <albado8@gmail.com>
2024-04-13 17:23:22 +0200

    Fájlba kiírás és Toast message

--- app/src/main/java/com/example/fishapi/FishAdapter.java
+++ app/src/main/java/com/example/fishapi/FishAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +31,11 @@ public class FishAdapter extends RecyclerView.Adapter<FishAdapter.FishViewHolder
     @Override
     public void onBindViewHolder(@NonNull FishViewHolder holder, int position) {
         Fish fish = fishList.get(position);
+
+        holder.itemView.setOnClickListener(v -> {
+            Toast.makeText(holder.itemView.getContext(), fish.getName(), Toast.LENGTH_SHORT).show();
+        });
+
         holder.bind(fish);
     }
 
